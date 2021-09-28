ea3c8efdd918d0592cfd8dc2a78c1acb9d98c278
xie.zheng <xie.zheng@17zuoye.com>
2021-09-28 12:26:51 +0800

    init commit

---
+++ Readme.md
@@ -0,0 +1,27 @@
+## Pipenv
+
+https://realpython.com/pipenv-guide/
+
+## Dataclass
+
+https://realpython.com/python-data-classes/
+
+## Book & Code
+
+### chapter 1
+
+https://www.cosmicpython.com/book/chapter_01_domain_model.html
+
+https://github.com/cosmicpython/code/tree/chapter_01_domain_model
+
+
+## Misc & Utilities
+
+### black
+
+https://pypi.org/project/black/
+
+### entr
+
+https://www.systutorials.com/docs/linux/man/1-entr/
+
---
+++ made_com/models.py
@@ -0,0 +1,38 @@
+from dataclasses import dataclass
+from typing import Optional, Set
+from datetime import date
+
+
+@dataclass(frozen=True)
+class OrderLine:
+    orderid: str
+    sku: str
+    qty: int
+
+
+class Batch:
+    def __init__(self, ref: str, sku: str, qty: int, eta: Optional[date]):
+        self.reference = ref
+        self.sku = sku
+        self.eta = eta
+        self._purchased_quantity = qty
+        self._allocations = set()  # type: Set[OrderLine]
+
+    def allocate(self, line: OrderLine):
+        if self.can_allocate(line):
+            self._allocations.add(line)
+
+    def deallocate(self, line):
+        if line in self._allocations:
+            self._allocations.remove(line)
+
+    @property
+    def allocated_quantity(self):
+        return sum([line.qty for line in self._allocations])
+
+    @property
+    def available_quantity(self):
+        return self._purchased_quantity - self.allocated_quantity
+
+    def can_allocate(self, line):
+        return self.sku == line.sku and self.available_quantity >= line.qty
